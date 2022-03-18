4a12a926f29f3f0b5b05332058e61c8a4e06c917
Aryan Pandey <margiaryan@gmail.com>
2022-03-19 04:11:18 +0530

    Nqueens added

--- com/aryan/backtracking/NQueens.java
+++ com/aryan/backtracking/NQueens.java
@@ -4,9 +4,15 @@ package com.aryan.backtracking;
  * NQueens
  */
 public class NQueens {
-
-	// n number of queens on board
+	// n number of queens on board, n x n chessboard
 	// no two queens attack each othher
+	// backtracking: Whatever changes made during recursion call, should be revert
+	// back when the recursion call gets end.
 	public static void main(String[] args) {
 	}
+
+	static void printN(int[][] board,int r, int c) {
+	    if ()
+	}
+
 }
